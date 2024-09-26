8cef56945b744b19be502589ca7f38936c3932cd
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-27 03:43:33 +0500

    Todo mod included

---
+++ todo/todo.go
@@ -0,0 +1,39 @@
+package todo
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Todo struct {
+	Text string `json:"text"`
+}
+
+func New(text string) Todo {
+	if text == "" {
+		panic("title and content are required")
+	}
+	return Todo{
+		Text: text,
+	}
+}
+
+// method to print note
+func (todo Todo) Print() {
+	fmt.Println(todo.Text)
+}
+
+//func to save note as a .json file
+
+func (todo Todo) SaveAsJSON() error {
+	fileName := "todo.json"
+
+	json, err := json.MarshalIndent(todo, "", " ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName+".json", json, 0644)
+}
